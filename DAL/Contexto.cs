59795fa9e9c09fbe98ef21d17f54a5e766049efc
Enmanuel Gonzalez Liriano <enmanuel_gonzalez1@ucne.edu.do>
2020-05-21 20:59:44 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using PrimerRegistroBlazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerRegistroBlazor.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data source = Data\PersonasDb");
+        }
+    }
+}
